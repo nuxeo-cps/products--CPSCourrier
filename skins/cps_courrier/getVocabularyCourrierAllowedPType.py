718d934354b90484382b9db9cac8475072430078
gracinet <devnull@localhost>
2006-02-22 04:59:23 +0000

    Blocker Bug: was not possible to create a mailbox

--- skins/cps_courrier/getVocabularyCourrierAllowedPType.py
+++ skins/cps_courrier/getVocabularyCourrierAllowedPType.py
@@ -10,9 +10,9 @@ l10n = context.translation_service
 res = [(item.getId(), l10n(item.Title()).encode('iso-8859-15'))
        for item in types]
 
-if key is None:
-    res.insert(0, ('', l10n('label_all').encode('iso-8859-15')))
-else:
-    res = [item[1] for item in res if item[0] == key][0]
+res.insert(0, ('', l10n('label_all').encode('iso-8859-15')))
+
+if key is not None:
+        res = [item[1] for item in res if item[0] == key][0]
 
 return res
