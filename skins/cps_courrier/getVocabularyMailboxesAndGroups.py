c3e6d8360eaf240eea20ca6037c60d2cd1c37d6f
gracinet <devnull@localhost>
2006-03-06 13:18:19 +0000

    Returns 'all' if not found.

--- skins/cps_courrier/getVocabularyMailboxesAndGroups.py
+++ skins/cps_courrier/getVocabularyMailboxesAndGroups.py
@@ -14,6 +14,10 @@ res = [(base_path+brain.relative_path, brain.Title) for brain in brains]
 res.insert(0, (base_path, l10n('cpscourrier_all_boxes').encode('iso-8859-15')))
 
 if key is not None:
-        res = [item[1] for item in res if item[0] == key][0]
+        match = [item[1] for item in res if item[0] == key]
+	if match: # found
+	    res = match[0]
+	else: # not found
+	    res = res[0][1]
 
 return res
