cb84f073462e95ad22cf417e74d5a47824b118c8
ogrisel <devnull@localhost>
2006-03-10 16:34:01 +0000

    added reply_to_incoming wf scripts that points to the corresponding module function

---
+++ profiles/default/workflows/incoming_mail_wf/scripts/reply_to_incoming.py
@@ -0,0 +1,13 @@
+## Script (Python) "reply_to_incoming"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=state_change
+##title=
+##
+from Products.CPSCourrier.workflows.scripts import reply_to_incoming
+outgoing_proxy = reply_to_incoming(state_change.object)
+url = "%s/cpsdocument_edit_form" % outgoing_proxy.absolute_url()
+context.REQUEST.RESPONSE.redirect(url)
