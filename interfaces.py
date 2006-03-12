f607c73d57101312c618d7d9fc71eb97030c0b07
gracinet <devnull@localhost>
2006-03-12 20:31:31 +0000

    stack interfaces and doctest wrapping

---
+++ interfaces.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2004-2005 Ministère de la justice <http://www.justice.gouv.fr/>
+# Copyright (c) 2004-2005 Capgemini <http://capgemini.com>
+# Copyright (c) 2004-2005 Nuxeo SARL <http://nuxeo.com>
+# Author: Anahide Tchertchian <at@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+
+from Products.CPSWorkflow.interfaces import IStackElement
+from Products.CPSWorkflow.interfaces import IHierarchicalWorkflowStack
+
+class IStackElementWithData(IStackElement):
+    """Stack Element With Data.
+
+    A stack element that can hold a mapping and implements default read and
+    write values.
+    """
+
+class IHierarchicalStackWithData(IHierarchicalWorkflowStack):
+    """ A hierarchical stack that can hold IStackElementWithData instances."""
+
+
+
+
