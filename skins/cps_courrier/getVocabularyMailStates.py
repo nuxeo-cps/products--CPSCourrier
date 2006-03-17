7c5adee5a026f43c387feb74bd54dd98a26975f9
gracinet <devnull@localhost>
2006-03-17 18:19:03 +0000

    Forgotten checkin

---
+++ skins/cps_courrier/getVocabularyMailStates.py
@@ -0,0 +1,28 @@
+##parameters=key=None
+#$Id$
+"""Return a portal type vocabulary, used as MethodVocabulary."""
+
+# TODO: use same lookup as for portal_types and then ask corresponding wfs
+
+# why a method if it's hardcoded? To put empty key *first
+l10n = context.translation_service
+vocitems = [(vkey, vkey) for vkey in ('received',
+				      'handled',
+				      'trash',
+				      'answering',
+				      'answered',
+				      'closed',
+				      'work',
+				      'locked',
+				      'draft',
+				      'validated',
+				      'sent')]
+res = [(item[0], l10n(item[1]).encode('iso-8859-15'))
+       for item in vocitems]
+
+res.insert(0, ('', l10n('label_all').encode('iso-8859-15')))
+
+if key is not None:
+        res = [item[1] for item in res if item[0] == key][0]
+
+return res
