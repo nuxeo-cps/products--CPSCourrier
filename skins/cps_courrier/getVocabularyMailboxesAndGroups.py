a88f46b542a784843027211b162c23a0c4465aac
ogrisel <devnull@localhost>
2006-04-19 17:41:19 +0000

    fixed \t mess

--- skins/cps_courrier/getVocabularyMailboxesAndGroups.py
+++ skins/cps_courrier/getVocabularyMailboxesAndGroups.py
@@ -14,10 +14,10 @@ res = [(base_path+brain.relative_path, brain.Title) for brain in brains]
 res.insert(0, (base_path, l10n('cpscourrier_all_boxes').encode('iso-8859-15')))
 
 if key is not None:
-        match = [item[1] for item in res if item[0] == key]
-	if match: # found
-	    res = match[0]
-	else: # not found
-	    res = res[0][1]
+    match = [item[1] for item in res if item[0] == key]
+    if match: # found
+        res = match[0]
+    else: # not found
+        res = res[0][1]
 
 return res
