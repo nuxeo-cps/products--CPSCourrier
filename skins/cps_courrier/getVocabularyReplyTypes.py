bed8bf0b9af77b91b69dc1461f409eabcd7de61d
gracinet <devnull@localhost>
2006-04-27 09:54:30 +0000

    Leverage folder_prefix in tabulars

--- skins/cps_courrier/getVocabularyReplyTypes.py
+++ skins/cps_courrier/getVocabularyReplyTypes.py
@@ -3,17 +3,16 @@
 """Return a portal type vocabulary, used as MethodVocabulary."""
 
 # reply types depend actually on location only
+# XXX should be done with a static voc and insertion of empty key but
+# this didn't work last time I tried
 
-utool = context.portal_url
-portal = utool.getPortalObject()
-base_path = '/' + utool.getPhysicalPath()[1] + '/'
 res = [('mailboxes', 'cpscourrier_simple_reply'),
        ('mail_templates', 'cpscourrier_mail_template')]
 
 l10n = context.translation_service
-res = [(base_path+id, l10n(title)) for id, title in res]
+res = [(id, l10n(title)) for id, title in res]
 
-res.insert(0, (base_path, l10n('label_all').encode('iso-8859-15')))
+res.insert(0, ('', l10n('label_all').encode('iso-8859-15')))
 
 if key is not None:
     match = [item[1] for item in res if item[0] == key]
