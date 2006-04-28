bf20619a21c9ce20bc582fe35ed6a478a51a64a5
ogrisel <devnull@localhost>
2006-04-28 10:53:39 +0000

    perf tuning

--- skins/cps_courrier/hasFlexibleFields.py
+++ skins/cps_courrier/hasFlexibleFields.py
@@ -7,4 +7,5 @@ flexible field"""
 import re
 regexp = '_f[0..9]+$'
 
-return int(bool([fid for fid in data if re.search(regexp, fid) is not None]))
+
+return int(bool(fid for fid in data if re.search(regexp, fid)))
