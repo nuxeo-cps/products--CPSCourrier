970d6e4e37cc6efd1f89a9c74798a364f1cdccc8
gracinet <devnull@localhost>
2006-05-29 23:45:11 +0000

    Error introduced with user-configurable batch sizes

--- widgets/dirsearch.py
+++ widgets/dirsearch.py
@@ -86,9 +86,9 @@ class DirectoryTabularWidget(TabularWidget):
         cpsdir = dtool[self.directory]
 
         entries = cpsdir.searchEntries(**query) # checks security
-        nb_pages = self.getNbPages(len(entries), b_size)
         (b_page, b_start, b_size) = self.getBatchParams(datastructure,
                                                         filters=filters)
+        nb_pages = self.getNbPages(len(entries), b_size)
         entries = entries[b_start:b_start+b_size]
 
         dms = (cpsdir._getDataModel(id) for id in entries)
