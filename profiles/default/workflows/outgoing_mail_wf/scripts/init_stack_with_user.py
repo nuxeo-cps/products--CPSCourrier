1cd38d0ed17dab70b1bc8460a251952810079996
ogrisel <devnull@localhost>
2006-06-07 15:08:34 +0000

    various wf / stacks permissions fixes in the outgoing wf

---
+++ profiles/default/workflows/outgoing_mail_wf/scripts/init_stack_with_user.py
@@ -0,0 +1,13 @@
+## Script (Python) "init_stack_with_user"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=state_change
+##title=
+##
+from Products.CPSCourrier.workflows.scripts import init_stack_with_user
+init_stack_with_user(state_change,
+                     'Pilots', prefix='courrier_',
+                     directive='handle')
