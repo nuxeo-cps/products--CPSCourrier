a22caa043f3d79ca985a9a507f7a2322b437f732
gracinet <devnull@localhost>
2006-06-20 12:13:13 +0000

    Don't use base_url as prefix for all searches anymore for performance (10x)

--- skins/cps_courrier/getVocabularyMailboxesAndGroups.py
+++ skins/cps_courrier/getVocabularyMailboxesAndGroups.py
@@ -18,7 +18,7 @@ l10n = context.translation_service
 res = [('/'.join((base_path, tree_el['rpath'])), tree_el['title'])
        for tree_el in tree.getList()
        if tree_el['portal_type'] in types]
-res.insert(0, (base_path, l10n('cpscourrier_all_boxes').encode('iso-8859-15')))
+res.insert(0, ('', l10n('cpscourrier_all_boxes').encode('iso-8859-15')))
 
 if key is not None:
     match = [item[1] for item in res if item[0] == key]
