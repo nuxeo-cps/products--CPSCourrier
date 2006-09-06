1d5e9802f101710d7fc6625fda25bab6e85a622a
gracinet <devnull@localhost>
2006-09-06 13:26:53 +0000

    Mailboxes voc checking the right roles for local addressbook creation/edition

---
+++ skins/cps_courrier/getVocabularyMailboxesOusForWrite.py
@@ -0,0 +1,49 @@
+##parameters=key=None
+#$Id$
+"""Return a portal type vocabulary, used as MethodVocabulary."""
+
+import logging
+from AccessControl import getSecurityManager
+logger = logging.getLogger('getVocabularyMailboxesOus')
+
+logger.debug('key=%s', key)
+
+l10n = context.translation_service
+lang = l10n.getLanguage()
+
+catalog = context.portal_catalog
+query = {'portal_type': 'Mailbox',
+         'language' : lang,
+         }
+
+if key is not None:
+    query['ou'] = key
+    brains = catalog(**query)
+    if len(brains) != 1:
+        return
+    return brains[0].Title
+
+if catalog.meta_type == 'CPS Lucene Catalog Tool':
+    title_sort = 'Title_sort'
+else:
+    title_sort = 'Title'
+
+query['sort-on'] = title_sort
+brains = catalog(**query)
+
+def intersects(list1, list2):
+    """No set in restricted code (sigh)."""
+    
+    for x in list1:
+        if x in list2:
+            return True
+    return False
+
+write_roles = ['Contributor', 'Supervisor', 'LocalManager',
+               'Injector', 'Manager']
+user = getSecurityManager().getUser()
+
+return [(brain.ou, brain.Title) for brain in brains
+        if intersects(write_roles,
+                      user.getRolesInContext(brain.getObject()))
+        ]
