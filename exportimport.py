075aef901e1e30442b88e67ab499fc708f1247d5
gracinet <devnull@localhost>
2006-09-13 14:37:38 +0000

    Wording

--- exportimport.py
+++ exportimport.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
-"""DgmeRel specific GenericSetup import/export"""
+"""CPSCourrier standalone specific GenericSetup import/export"""
 
 from Products.CMFCore.utils import getToolByName
 
