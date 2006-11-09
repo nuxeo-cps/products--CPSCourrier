0f64cfcdb1c245e80720ac0da0f3252f3531b3ba
gracinet <devnull@localhost>
2006-11-09 18:09:18 +0000

    Leverage [49959]

--- browser/batchperformview.py
+++ browser/batchperformview.py
@@ -17,13 +17,12 @@
 #
 # $Id$
 
-import transaction
-
-from urllib import urlencode
 from logging import getLogger, DEBUG
 
+import transaction
+
 from Acquisition import aq_inner, aq_parent
-from OFS.CopySupport import CopyError
+
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.WorkflowCore import WorkflowException
 from Products.CPSCore.EventServiceTool import getPublicEventService
@@ -31,205 +30,18 @@ from Products.CPSCourrier.workflows.scripts import reply_to_incoming
 from Products.CPSUtil.session import sessionHasKey
 from Products.CPSUtil.timer import Timer
 
-from reuseanswerview import ReuseAnswerView
+from Products.CPSDashboards.browser.batchperformview import BatchPerformView as BaseBatchView
+
+from Products.CPSDashboards.browser.searchview import SearchView
 
 logger = getLogger('CPSCourrier.browser.batchperformview')
 
-_SESSION_KEY = "CPSCOURRIER_BATCH_PERFORM"
+class BatchPerformView(BaseBatchView, SearchView):
+    layout_id = 'mail_search_answers'
 
-class BatchPerformView(ReuseAnswerView):
+    session_key = "CPSCOURRIER_BATCH_PERFORM"
 
-    transition = None # id of the wf action to be performed
-    rapth = None # rpath of the reply to reuse if answering
-    rpaths = () # rpaths of mails as target of the transition
-
-    submit_button_prefix = "cpscourrier_batch_"
-    # namespace prefix for all the submit buttons that call this view from a
-    # mailbox or a dashboard listing
-
-    #
-    # Helpers to maintain current session
-    #
-
-    def _storeDataInSession(self, rpaths, transition_id):
-        """Store data in a session to allow for multi screen action"""
-        request = self.request
-        if not sessionHasKey(request, _SESSION_KEY):
-            request.SESSION[_SESSION_KEY] = {}
-        rpath = getToolByName(self.context, 'portal_url').getRpath(self.context)
-        data = {
-            'transition_id': transition_id,
-            'rpaths': rpaths,
-        }
-        request.SESSION[_SESSION_KEY][rpath] = data
-
-    def _readDataFromSession(self):
-        """Read the request session to find stored rpaths"""
-        request = self.request
-        if not sessionHasKey(request, _SESSION_KEY):
-            return [], None
-        rpath = getToolByName(self.context, 'portal_url').getRpath(self.context)
-        data = request.SESSION[_SESSION_KEY].get(rpath, {})
-        return data.get('rpaths', []), data.get('transition_id')
-
-    def _expireSession(self):
-        """Expire rpaths info related to the current mailbox"""
-        request = self.request
-        if not sessionHasKey(request, _SESSION_KEY):
-            return
-        rpath = getToolByName(self.context, 'portal_url').getRpath(self.context)
-        del request.SESSION[_SESSION_KEY][rpath]
-
-    def _filterMatchingRpaths(self, transition, rpaths):
-        """Helper method to filter out non p-matching rpaths"""
-        portal = getToolByName(self.context, 'portal_url').getPortalObject()
-        wftool = getToolByName(self.context, 'portal_workflow')
-        filtered = []
-        for rpath in rpaths:
-            proxy = portal.unrestrictedTraverse(rpath)
-            for wf in wftool.getWorkflowsFor(proxy):
-                if wf.isActionSupported(proxy, transition):
-                    filtered.append(rpath)
-        return filtered
-
-    def _getSessionData(self):
-        """Process the request to extract the list of rpaths and transition id
-
-        Also take care of storing/reading/updating the session if necessary
-        """
-        form = self.request.form
-
-        # extract rpaths info from the request
-
-        if 'ids' in form:
-            utool = getToolByName(self.context, "portal_url")
-            folder_rpath = utool.getRpath(self.context.aq_inner)
-            rpaths =  ['%s/%s' % (folder_rpath, id) for id in form['ids']]
-        else:
-            rpaths = form.get('rpaths', ())
-
-        # extract transition id info from request
-
-        transitions = [key for key in form
-                           if key.startswith(self.submit_button_prefix)]
-        if len(transitions) > 1:
-            raise ValueError("Got more than one transition to perform")
-        if transitions:
-            transition = transitions[0][len(self.submit_button_prefix):]
-        else:
-            transition = None
-
-        if 'answer_submit' in form:
-            transition =  'answer'
-
-        if transition not in ('cut', 'copy', 'paste', None):
-            # filter out non matching rpaths
-            rpaths = self._filterMatchingRpaths(transition, rpaths)
-
-        # session management:
-        #  - store data read from the request
-        #  - read missing data previously stored
-
-        # are we at the first call of the batch form or inside a multi screen
-        # session?
-        init_call = False
-        for key in form:
-            if key.startswith(self.submit_button_prefix):
-                init_call = True
-                break
-
-        if not rpaths and not init_call:
-            # nothing was provided in the request, we are probably in a multi
-            # screens session:
-            # try to see if data was previously stored in the session
-            rpaths, transition = self._readDataFromSession()
-        else:
-            # rpaths were directly provided in the request, store them in
-            # cookies for later reuse
-            self._storeDataInSession(rpaths, transition)
-
-        logger.debug('rpaths: %s, transition_id: %s', rpaths, transition)
-        return rpaths, transition
-
-    #
-    # Cut copy paste management
-    #
-
-    def _doRedirect(self, psm):
-        url = "%s?%s" % (self.context.absolute_url(),
-                         urlencode({'portal_status_message': psm}))
-        self.request.RESPONSE.redirect(url)
-        # return a flag that tells the template not to render the rest of the
-        # view since we don't need it
-        return 'do_redirect'
-
-    def _doCutCopyPaste(self):
-        """Special handling of cut/copy/paste actions
-
-        This are not actual transitions and require special treatment
-        """
-        if self.transition == 'paste':
-            if self.context.cb_dataValid:
-                cp = self.request['__cp']
-                try:
-                    result = self.context.manage_CPSpasteObjects(cp)
-                    for id in [ob['new_id'] for ob in result]:
-                        ob = getattr(self.context, id)
-                        evtool = getPublicEventService(self.context)
-                        evtool.notifyEvent('workflow_cut_copy_paste', ob, {})
-                    psm = 'psm_item(s)_pasted'
-                except CopyError:
-                    psm = 'psm_copy_or_cut_at_least_one_document'
-                except WorkflowException:
-                    psm = 'psm_operation_not_allowed'
-            else:
-                psm = 'psm_copy_or_cut_at_least_one_document'
-        else:
-            # copy or cut
-            ids = [rpath.rsplit('/', 1)[1] for rpath in self.rpaths]
-            if ids:
-                if self.transition == 'cut':
-                    self.context.manage_CPScutObjects(ids, self.request)
-                    psm = 'psm_item(s)_cut'
-                if self.transition == 'copy':
-                    self.context.manage_CPScopyObjects(ids, self.request)
-                    psm = 'psm_item(s)_copied'
-            else:
-                psm = 'psm_select_at_least_one_document'
-
-        return self._doRedirect(psm)
-
-    #
-    # View API for the template
-    #
-
-    def dispatchSubmit(self):
-        """Process the POST request of the mailbox/dashboard listing views
-
-        There are mainly two cases:
-         - batch reply: reuse the reuse answer search form to trigger the
-           reply wf script with the selected template as model
-         - other batch transitions with a classical confirmation form
-        """
-        # compute the list of incoming mail rpath and store them as attribute
-        # of the view instance
-        self.rpaths, self.transition = self._getSessionData()
-
-        if not self.transition:
-            raise ValueError('No transition specified')
-
-        if self.transition in ('cut', 'copy', 'paste'):
-            # special actions that are not handled by actual workflow
-            # transitions
-            return self._doCutCopyPaste()
-
-        if not self.rpaths:
-            return self._doRedirect('psm_select_at_least_one_item')
-
-        trigger_transition = self.request.form.get('trigger_transition', None)
-        if trigger_transition is not None:
-            # handle the redirection and the psm
-            return self.batchTriggerTransition(trigger_transition)
+    submit_button_prefix = "cpsdashboards_batch_"
 
     def getMailInfo(self):
         """Compute data related to the rpath"""
@@ -249,8 +61,8 @@ class BatchPerformView(ReuseAnswerView):
 
     def showSearchAnswerForm(self):
         """Decide whether we should display the transition confirmation form"""
-        if self.transition != 'answer':
-            # No search for non answer transitions
+        if self.action != 'answer':
+            # No search for non answer actions
             return False
         if 'rpath' in self.request.form:
             # The template answer as already been chosen
@@ -259,8 +71,8 @@ class BatchPerformView(ReuseAnswerView):
 
     def showConfirmationForm(self):
         """Decide whether we should display the transition confirmation form"""
-        if self.transition != 'answer':
-            # Non answer transitions directly need the final confirmation form
+        if self.action != 'answer':
+            # Non answer actions directly need the final confirmation form
             return True
         if 'rpath' in self.request.form:
             # The answering template is chosen, show the confirmation form
@@ -372,6 +184,3 @@ class BatchPerformView(ReuseAnswerView):
         body += '\n\n%s\n\n-- \n%s' % (foa , signature)
         fake_original_msg = mcat('cpscourrier_fake_original_msg')
         return "%s\n\n%s" % (body,  fake_original_msg)
-
-
-
