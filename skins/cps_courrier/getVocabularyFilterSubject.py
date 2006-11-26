48e1f4d0ac67b3762276a4060ce777086c64c056
gracinet <devnull@localhost>
2006-11-26 23:27:36 +0000

    updated docstring

--- skins/cps_courrier/getVocabularyFilterSubject.py
+++ skins/cps_courrier/getVocabularyFilterSubject.py
@@ -1,9 +1,9 @@
 ##parameters=key=None, **kw
 #$Id$
-"""Return a portal type vocabulary, used as MethodVocabulary."""
+"""Subject voc with empty key.
 
 # just because one cannot add empty value for static vocabularies
-# (these thing should be handled by Select Widget, not the voc  anyway.
+# (these things can now be handled by Select Widget)."""
 
 basevoc = context.portal_vocabularies['subject_voc']
 if key is None:
