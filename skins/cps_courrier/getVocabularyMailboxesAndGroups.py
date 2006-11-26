effff15ad45f80af93517bb101cc54c2e8c11080
gracinet <devnull@localhost>
2006-11-26 23:22:30 +0000

    Updated to [50302] and fixed docstring

--- skins/cps_courrier/getVocabularyMailboxesAndGroups.py
+++ skins/cps_courrier/getVocabularyMailboxesAndGroups.py
@@ -1,6 +1,8 @@
 ##parameters=key=None
 #$Id$
-"""Return a portal type vocabulary, used as MethodVocabulary."""
+"""Return the vocabulary rpath -> title of all mailboxes.
+
+Used for filters in dashboards and advanced search."""
 
 # We use the list of types allowed in container as searchable types
 
@@ -10,14 +12,14 @@ logger = logging.getLogger('getVocabularyMailboxesAndGroups')
 logger.debug('key=%s', key)
 
 tree = context.portal_trees.mailboxes
-portal = context.portal_url.getPortalObject()
-base_path = '/'.join(portal.getPhysicalPath())
 types = ['Mailbox', ]
 
 l10n = context.translation_service
-res = [('/'.join((base_path, tree_el['rpath'])), tree_el['title'])
+res = [(tree_el['rpath'], tree_el['title'])
        for tree_el in tree.getList()
        if tree_el['portal_type'] in types]
+
+# XXX should go now that Select Widget can cope
 res.insert(0, ('', l10n('cpscourrier_all_boxes').encode('iso-8859-15')))
 
 if key is not None:
