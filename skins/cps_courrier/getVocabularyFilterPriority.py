1260c4e87fa2779116951d57ddb74bd21e8fc320
gracinet <devnull@localhost>
2006-11-26 23:29:30 +0000

    Updated docstring for soon-to-die method voc

--- skins/cps_courrier/getVocabularyFilterPriority.py
+++ skins/cps_courrier/getVocabularyFilterPriority.py
@@ -1,9 +1,6 @@
 ##parameters=key=None, **kw
 #$Id$
-"""Return a portal type vocabulary, used as MethodVocabulary."""
-
-# just because one cannot add empty value for static vocabularies
-# (these thing should be handled by Select Widget, not the voc  anyway.
+"""Empty key Method Vocab. Remove and use widget-level empty_key."""
 
 basevoc = context.portal_vocabularies['mail_priority']
 if key is None:
